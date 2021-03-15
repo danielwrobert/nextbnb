ae94c313337a5f7b3fef30615a5d5f7570d94724
Daniel W. Robert <danielwrobert@gmail.com>
2021-03-15 15:05:43 -0400

    Complete section 2, lesson 4. E/O section 2.

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import houses from '../../houses.js';
+import Layout from '../../components/Layout';
 
 export async function getServerSideProps({ query }) {
 	const { id } = query;
@@ -13,15 +14,19 @@ export async function getServerSideProps({ query }) {
 
 export default function House(props) {
 	return (
-		<div>
-			<Head>
-				<title>{props.house.title}</title>
-			</Head>
-			<img src={props.house.picture} width="100%" alt="House picture" />
-			<p>
-				{props.house.type} - {props.house.town}
-			</p>
-			<p>{props.house.title}</p>
-		</div>
+		<Layout
+			content={
+				<div>
+					<Head>
+						<title>{props.house.title}</title>
+					</Head>
+					<img src={props.house.picture} width="100%" alt="House picture" />
+					<p>
+						{props.house.type} - {props.house.town}
+					</p>
+					<p>{props.house.title}</p>
+				</div>
+			}
+		/>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
 import houses from '../houses.js';
+import Layout from '../components/Layout';
 import House from '../components/House';
 
-export default function Home() {
-	return (
-		<div>
-			<h2>Places to stay</h2>
+const content = (
+	<div>
+		<h2>Places to stay</h2>
 
-			<div className="houses">
-				{houses.map((house, index) => {
-					return <House key={index} {...house} />;
-				})}
-			</div>
-
-			<style jsx>{`
-				.houses {
-					display: grid;
-					grid-template-columns: 49% 49%;
-					grid-template-rows: 300px 300px;
-					grid-gap: 2%;
-				}
-			`}</style>
+		<div className="houses">
+			{houses.map((house, index) => {
+				return <House key={index} {...house} />;
+			})}
 		</div>
-	);
+
+		<style jsx>{`
+			.houses {
+				display: grid;
+				grid-template-columns: 49% 49%;
+				grid-template-rows: 300px 300px;
+				grid-gap: 2%;
+			}
+		`}</style>
+	</div>
+);
+
+export default function Home() {
+	return <Layout content={content} />;
 }
