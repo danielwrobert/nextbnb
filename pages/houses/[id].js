5dd571330ff36475b29f1a11c2a6d59f0bff9925
Daniel W. Robert <danielwrobert@users.noreply.github.com>
2021-04-19 14:37:26 -0400

    Complete lessons 1-2 of Date Picker section.

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -7,8 +7,8 @@ export async function getServerSideProps({ query }) {
 
 	return {
 		props: {
-			house: houses.filter((house) => house.id === parseInt(id))[0],
-		},
+			house: houses.filter((house) => house.id === parseInt(id, 10))[0]
+		}
 	};
 }
 
@@ -16,15 +16,31 @@ export default function House(props) {
 	return (
 		<Layout
 			content={
-				<div>
+				<div className="container">
 					<Head>
 						<title>{props.house.title}</title>
 					</Head>
-					<img src={props.house.picture} width="100%" alt="House picture" />
-					<p>
-						{props.house.type} - {props.house.town}
-					</p>
-					<p>{props.house.title}</p>
+					<article>
+						<img src={props.house.picture} width="100%" alt="House" />
+						<p>
+							{props.house.type} - {props.house.town}
+						</p>
+						<p>{props.house.title}</p>
+					</article>
+					<aside></aside>
+
+					<style jsx>{`
+						.container {
+							display: grid;
+							grid-template-columns: 60% 40%;
+							grid-gap: 30px;
+						}
+
+						aside {
+							border: 1px solid #ccc;
+							padding: 20px;
+						}
+					`}</style>
 				</div>
 			}
 		/>
