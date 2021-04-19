f3913ba6e6335d4b27b8353562684930af43c42e
Daniel W. Robert <danielwrobert@gmail.com>
2021-04-19 15:41:11 -0400

    Complete section 3, lesson 5.

--- components/DateRangePicker.js
+++ components/DateRangePicker.js
@@ -1,16 +1,64 @@
+import { useState } from 'react';
+
 import DayPickerInput from 'react-day-picker/DayPickerInput';
+import dateFnsFormat from 'date-fns/format';
+import dateFnsParse from 'date-fns/parse';
+import { DateUtils } from 'react-day-picker';
+
 import 'react-day-picker/lib/style.css';
 
+const parseDate = (str, format, locale) => {
+	const parsed = dateFnsParse(str, format, new Date(), { locale });
+	return DateUtils.isDate(parsed) ? parsed : null;
+};
+
+const formatDate = (date, format, locale) => dateFnsFormat(date, format, { locale });
+
+const format = 'dd MMM yyyy';
+
 export default function DateRangePicker() {
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
 	return (
 		<div className="date-range-picker-container">
 			<div>
 				<label>From:</label>
-				<DayPickerInput />
+				<DayPickerInput
+					formatDate={formatDate}
+					format={format}
+					parseDate={parseDate}
+					placeholder={`${dateFnsFormat(new Date(), format)}`}
+					dayPickerProps={{
+						modifiers: {
+							disabled: {
+								before: new Date(),
+							},
+						},
+					}}
+					onDayChange={(day) => {
+						setStartDate(day);
+					}}
+				/>
 			</div>
 			<div>
 				<label>To:</label>
-				<DayPickerInput />
+				<DayPickerInput
+					formatDate={formatDate}
+					format={format}
+					parseDate={parseDate}
+					placeholder={`${dateFnsFormat(new Date(), format)}`}
+					dayPickerProps={{
+						modifiers: {
+							disabled: {
+								before: new Date(),
+							},
+						},
+					}}
+					onDayChange={(day) => {
+						setEndDate(day);
+					}}
+				/>
 			</div>
 			<style jsx>
 				{`
